73907425ae63387a8fe5c025145fbc284f44d31a
quoc-quy <quyleom10@gmail.com>
2025-11-25 19:57:11 +0700

    làm authMiddleware để xác thực người dùng

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,10 @@
+export const authMe = async (req, res) => {
+    try {
+        const user = req.user; // lấy user từ authMiddleware
+
+        return res.status(200).json({ user });
+    } catch (error) {
+        console.error("lỗi khi gọi authMe: ", error);
+        return res.status(500).json({ message: "Lộ hệ thống" });
+    }
+};
