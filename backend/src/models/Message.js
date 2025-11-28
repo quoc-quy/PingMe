32400321b8d8a6c88a7106678737609bd9170f4e
quoc-quy <quyleom10@gmail.com>
2025-11-28 12:38:29 +0700

    hoàn thành phần model

---
+++ backend/src/models/Message.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+    {
+        conversationId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+            required: true,
+        },
+
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        content: {
+            type: String,
+            trim: true,
+        },
+
+        imgUrl: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+messageSchema.index({ conversationId: 1 }, { createdAt: -1 });
+
+export default mongoose.model("Message", messageSchema);
