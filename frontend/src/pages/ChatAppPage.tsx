7c30137e7c4aace3400bcc50bdad875b141371be
quoc-quy <quyleom10@gmail.com>
2026-01-02 20:37:20 +0700

    feat: Add sidebar

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,29 +1,15 @@
-import Logout from "@/components/auth/Logout";
-import { Button } from "@/components/ui/button";
-import api from "@/lib/axios";
-import { useAuthStore } from "@/stores/useAuthStore";
-import React, { use } from "react";
-import { toast } from "sonner";
+import ChatWindowLayout from "@/components/chat/ChatWindowLayout";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 const ChatAppPage = () => {
-    const user = useAuthStore((s) => s.user);
-
-    const handleOnclick = async () => {
-        try {
-            await api.get("/users/test", { withCredentials: true });
-            toast.success("Ok");
-        } catch (error) {
-            console.error(error);
-            toast.error("not ok");
-        }
-    };
     return (
-        <div>
-            {user?.username}
-            <Logout />
-
-            <Button onClick={handleOnclick}>test</Button>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <div className="flex h-screen w-full p-2">
+                <ChatWindowLayout />
+            </div>
+        </SidebarProvider>
     );
 };
 
