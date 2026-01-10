77bc824a3a22d3dcd7b7655d23cc67526640e81e
quoc-quy <quyleom10@gmail.com>
2026-01-10 15:40:25 +0700

    feat: Do ChatWindowHeader

--- frontend/src/components/chat/ChatWelcomeScreen.tsx
+++ frontend/src/components/chat/ChatWelcomeScreen.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { SidebarInset } from "../ui/sidebar";
+import ChatWindowHeader from "./ChatWindowHeader";
 
 const ChatWelcomeScreen = () => {
-    return <div>ChatWelcomeScreen</div>;
+    return (
+        <SidebarInset className="flex w-full h-full bg-transparent">
+            <ChatWindowHeader />
+
+            <div className="flex bg-primary-foreground rounded-2xl flex-1 items-center justify-center">
+                <div className="text-center">
+                    <div className="size-24 mx-auto mb-6 bg-gradient-chat rounded-full flex items-center justify-center shadow-glow pulse-ring">
+                        <span className="text-3xl">🗯️</span>
+                    </div>
+                    <h2 className="text-2xl font-bold mb-2 bg-gradient-chat bg-clip-text text-transparent">
+                        Chào mừng bạn đến với PingMe!
+                    </h2>
+                    <p className="text-muted-foreground">
+                        Hãy bắt đầu cuộc trò chuyện ngay bây giờ!
+                    </p>
+                </div>
+            </div>
+        </SidebarInset>
+    );
 };
 
 export default ChatWelcomeScreen;
